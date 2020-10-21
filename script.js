113f2d1a612894212d8e56ffb5e485680f56e392
lukefw <48167291+lukefw@users.noreply.github.com>
2020-10-21 00:16:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,115 @@
-var character = 
-document.getElementById("character");
+var character = document.getElementById("character");
+var snake = document.getElementById("snake");
+var scorpion = document.getElementById("scorpion");
+var a = 0;
+var jumps = 0;
 
-var block = document.getElementById("block");
-document.addEventListener('onclick', jump);
 
-var counter = 0;
+/*var firebaseConfig = {
+    apiKey: "AIzaSyDPdoT2ThRcSpCr8UipqrsD_u-87sTKyZg",
+    authDomain: "ninjajump-5ad8e.firebaseapp.com",
+    databaseURL: "https://ninjajump-5ad8e.firebaseio.com",
+    projectId: "ninjajump-5ad8e",
+    storageBucket: "ninjajump-5ad8e.appspot.com",
+    messagingSenderId: "346441750488",
+    appId: "1:346441750488:web:88db99b3c2287a635444ad"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-function jump(){
+let myDatabase = firebase.database().child(getUniqueID());
+
+myDatabase.child("Username").on("child_added", function(dataSnapShotHandler){
+    let Data = dataSnapShotHandler.val();
+    console.log(Data);
+    let $new = document.createElement("li");
+    $newItem.innerText = `Username: ${Data.time}`;
+    document.getElementById("list").appendChild($new);
+});
+
+document.getElementById("Enter").addEventListener("click", function(){
+    let clickTime = document.getElementById("username").value; 
+    let newClick = yourDatabase.child("Username").push();
+    newClick.set({User: clickTime}); 
+  });
+
+*/
+
+
+function start(){
+
+
+
+
+}
+
+//document.getElementById("start").addEventListener("onclick", start);
+
+function up(evt){
+    console.log(evt);
+    jumps += 1;
     if(character.classList == "animate"){return}
     character.classList.add("animate");
     
     
     setTimeout(function(){
         character.classList.remove("animate");
-    },400)
+    },500)
 }
+//make jump incrementer
+document.addEventListener("keydown", up);
 
-document.addEventListener("keydown", jump);
-
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20  && blockLeft>-10 && characterTop >= 200){
-        block.style.animation = "none";
-        block.style.display = "none";
-        alert("Game over! Number of jumps: " + Math.floor(counter/50));
-        counter = 0;
-        block.style.animation = "block 0.5s infinite linear";
+var endGame = setInterval(function(){
+    var characterBorder = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var snakeBorder = parseInt(window.getComputedStyle(snake).getPropertyValue("left"));
+    var scorpionBorder = parseInt(window.getComputedStyle(scorpion).getPropertyValue("left"));
+    if(snakeBorder<30  && snakeBorder>-10 && characterBorder >= 315){
+        snake.style.animation = "none";
+        snake.style.display = "none";
+        alert("Game over! Number of jumps: " + jumps);
+        a = 0;
+        snake.style.animation = "attack 1s infinite linear";
+        
+    }
+    else if(scorpionBorder<30  && scorpionBorder>-10 && characterBorder >= 315){
+        scorpion.style.animation = "none";
+        scorpion.style.display = "none";
+        alert("Game over! Number of jumps: " + jumps);
+        a = 0;
+        scorpion.style.animation = "zip 1.3s infinite linear";
+        
     }
     else{
-        counter++
-        document.getElementById("jumpSpan").innerHTML = Math.floor(counter/50);
+        a++
+        document.getElementById("jumpSpan").innerHTML = jumps;
     }
 
 },10)
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDPdoT2ThRcSpCr8UipqrsD_u-87sTKyZg",
+    authDomain: "ninjajump-5ad8e.firebaseapp.com",
+    databaseURL: "https://ninjajump-5ad8e.firebaseio.com",
+    projectId: "ninjajump-5ad8e",
+    storageBucket: "ninjajump-5ad8e.appspot.com",
+    messagingSenderId: "346441750488",
+    appId: "1:346441750488:web:88db99b3c2287a635444ad"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let myDatabase = firebase.database().child(getUniqueId());
+
+myDatabase.child("Username").on("child_added", function(dataSnapShotHandler){
+    let Data = dataSnapShotHandler.val();
+    console.log(Data);
+    let $new = document.createElement("li");
+    $newItem.innerText = `Username: ${Data.time}`;
+    document.getElementById("list").appendChild($new);
+});
+
+document.getElementById("Enter").addEventListener("click", function(){
+    let clickTime = document.getElementById("username").value; 
+    let newClick = yourDatabase.child("Username").push();
+    newClick.set({User: clickTime}); 
+  });
